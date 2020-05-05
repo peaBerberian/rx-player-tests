c5103a82b21c53e127e6cffc76395fb921bf3de0
peaBerberian <pea.berberian@gmail.com>
2020-05-05 11:51:43 +0200

    fix build scripts to take into consideration the new src directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 mode: "production",
-entry: "./js/main.js",
+entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
